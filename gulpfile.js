0ca1c79eadae4b0ccc3399e9ff3436b689a6d587
Warren Haskins <warrenhaskins@gmail.com>
2015-01-05 10:48:57 +0000

    First Commit BOOM

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var  gulp           = require('gulp'),
+     plumber 		= require('gulp-plumber'),
+     livereload     = require('gulp-livereload'),
+     nunjucksRender = require('gulp-nunjucks-render'),
+     webserver 	= require('gulp-webserver');
+
+nunjucksRender.nunjucks.configure(['templates/','templates/partials/','css/']);
+
+/*
+|--------------------------------------------------------------------------
+| Error Catching
+|--------------------------------------------------------------------------
+*/
+
+var onError = function (err) {  
+	console.log(err);
+	this.emit('end');
+};
+
+
+/*
+|--------------------------------------------------------------------------
+| Build Nunjucks
+|--------------------------------------------------------------------------
+*/
+
+gulp.task('render', function () {
+	gulp.src('templates/index.html')
+	.pipe(plumber({
+		errorHandler: onError
+	}))
+	.pipe(nunjucksRender())
+	.pipe(gulp.dest('build'));
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Watch Task
+|--------------------------------------------------------------------------
+*/
+
+gulp.task('watch', function() {
+     gulp.watch(['templates/*.html','css/*.css'], ['render']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('build')
+	.pipe(webserver({
+		livereload: true,
+		directoryListing: false,
+		open: true
+	}));
+});
+
+
+gulp.task('run', ['webserver','watch']);
